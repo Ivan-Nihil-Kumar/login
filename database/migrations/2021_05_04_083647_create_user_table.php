6c9a01ef9173b7794d207e56853581bb34060a12
Ivan <ivan.d@raonebytes.com>
2021-06-03 11:10:46 +0530

    Initial commit

---
+++ database/migrations/2021_05_04_083647_create_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',250);
+            $table->string('password',250);
+            $table->string('email',250);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
